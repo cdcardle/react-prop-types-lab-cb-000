14817c02a60c979ca561711fda1b69ea175ff6d8
Chris Cardle <cdcardle@gmail.com>
2019-02-05 18:25:58 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -31,7 +31,9 @@ Product.propTypes = {
 
 function weightAllowed(props, propName, componentName) {
   const value = props[propName];
-  if (isNaN(value)) {
+  if (value === undefined) {
+    return new Error(propName + 'cannot be undefined.  Validation failed.')
+  } else if (isNaN(value)) {
     return new Error(propName + 'is not a number.  Validation failed.');
   } else if (value < 80 && value > 300) {
     return new Error(propName + 'should be between 80 and 300.  Validation failed.')
