bb06f614ff7a53b09c817fed70279d3f0759d8bd
Chris Cardle <cdcardle@gmail.com>
2019-02-05 18:21:08 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -31,7 +31,7 @@ Product.propTypes = {
 
 function weightAllowed(props, propName, componentName) {
   const value = props[propName];
-  if (!(value >= 80 && value <= 300)) {
+  if (isNaN(value)) {
     return new Error(
       'Invalid prop `' + propName + '` supplied to' +
         ' `' + componentName + '`. Validation failed.'
