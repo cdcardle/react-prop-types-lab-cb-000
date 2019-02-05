b332cba046a64e2ace4a66c9d06dec944615dcb2
Chris Cardle <cdcardle@gmail.com>
2019-02-05 18:16:16 +0000

    Automatically backed up by Learn

--- src/components/Product.js
+++ src/components/Product.js
@@ -26,12 +26,14 @@ Product.propTypes = {
   producer: PropTypes.string,
   hasWatermark: PropTypes.bool,
   color: PropTypes.oneOf(['white', 'eggshell-white', 'salmon']).isRequired,
-  weight: weightAllowed
+  weight: weightAllowed.isRequired
 }
 
 function weightAllowed(props, propName, componentName) {
   let value = props[propName];
-  if ((value >= 80 && value <= 300)) {
+  if (value >= 80 && value <= 300) {
+    return null;
+  } else {
     return new Error(
       'Validation Failed.  `' + propName + '` needs to be a number between 80 and 300.'
     );
